39e61e927a3fd8de34d2cc0de86edabcd151cd1a
angel escobedo <153581828+jaypeanutt@users.noreply.github.com>
2024-06-24 21:12:28 -0700

    added database

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,23 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {foreignKey: "category_id" })
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {foreignKey: "category_id"})
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+ foreignKey: "product_id", 
+ through: ProductTag
+})
+
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id", 
+  through: ProductTag 
+})
 
 module.exports = {
   Product,
